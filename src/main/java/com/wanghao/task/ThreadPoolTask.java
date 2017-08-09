f25c8a758c584a5810273f982118b909d9478a92
xiangxianzui <wh15895877701@126.com>
2017-08-09 17:19:11 +0800

    update .gitignore

---
+++ src/main/java/com/wanghao/task/ThreadPoolTask.java
@@ -0,0 +1,40 @@
+package com.wanghao.task;
+
+import java.io.Serializable;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by wanghao on 8/9/17.
+ */
+public class ThreadPoolTask implements Callable<String>, Serializable {
+    private static final long serialVersionUID = 0;
+
+    // 保存任务所需要的数据
+    private Object threadPoolTaskData;
+
+    private static int consumeTaskSleepTime = 2000;
+
+    public ThreadPoolTask(Object tasks) {
+        this.threadPoolTaskData = tasks;
+    }
+
+    public synchronized String call() throws Exception {
+        // 处理一个任务，这里的处理方式太简单了，仅仅是一个打印语句
+        System.out.println("开始执行任务：" + threadPoolTaskData);
+        String result = "";
+        // 便于观察，等待一段时间
+        try {
+            // long r = 5/0;
+            for (int i = 0; i < 100000000; i++) {
+
+            }
+            result = "OK";
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = "ERROR";
+        }
+        threadPoolTaskData = null;
+        return result;
+    }
+
+}
---
+++ src/test/java/com/wanghao/TestThreadPool.java
@@ -0,0 +1,50 @@
+package com.wanghao;
+
+/**
+ * Created by wanghao on 8/9/17.
+ */
+
+import com.wanghao.task.StartTaskThread;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+// 指定的运行runner，并且把你所指定的Runner作为参数传递给它
+@ContextConfiguration(locations = "classpath*:spring.xml")
+public class TestThreadPool extends AbstractJUnit4SpringContextTests {
+    private static int produceTaskSleepTime = 10;
+
+    private static int produceTaskMaxNumber = 1000;
+
+    @Autowired
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    public ThreadPoolTaskExecutor getThreadPoolTaskExecutor() {
+        return threadPoolTaskExecutor;
+    }
+
+    public void setThreadPoolTaskExecutor(
+            ThreadPoolTaskExecutor threadPoolTaskExecutor) {
+        this.threadPoolTaskExecutor = threadPoolTaskExecutor;
+    }
+
+    @Test
+    public void testThreadPoolExecutor() {
+        for (int i = 1; i <= produceTaskMaxNumber; i++) {
+            try {
+                Thread.sleep(produceTaskSleepTime);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            new Thread(new StartTaskThread(threadPoolTaskExecutor, i)).start();
+        }
+
+    }
+
+}
+
